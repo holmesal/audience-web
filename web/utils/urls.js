ef286f6b2a0e36ca7afcc49942d0161e4c915d14
Alonso Holmes <hello@alonso.io>
2016-03-25 15:24:07 -0700

    webapp now also knows about staging

--- web/utils/urls.js
+++ web/utils/urls.js
@@ -2,8 +2,10 @@ const {hostname, protocol} = window.location;
 
 const localHostnames = ['localhost', '172.20.10.4'];
 
+export const isStaging = hostname.toLowerCase().indexOf('staging') != -1;
+
 export const API_ROOT = localHostnames.indexOf(hostname) === -1 ?
-    'https://podcastfoo.herokuapp.com' :
+    (isStaging ? 'https://podcastfoo-staging.herokuapp.com' : 'https://podcastfoo.herokuapp.com') :
     `http://${hostname}:5000`;
 
 export const S3_BUCKET = localHostnames.indexOf(hostname) === -1 ?

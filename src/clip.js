23b1dae62b041c034a7af0d0d459f9ec3400aae3
Alonso Holmes <hello@alonso.io>
2016-03-25 15:41:15 -0700

    removed og:url tag

--- src/clip.js
+++ src/clip.js
@@ -38,7 +38,6 @@ let template = (clip) => {
           <meta property="fb:app_id"       content="929053407186638" />
           <meta property="og:type"         content="music.song" />
           <meta property="og:title"        content="Clip from ${clip.episode.title}" />
-          <meta property="og:url"          content="${mp3Url}" />
           <meta property="og:image"        content="${clip.episode.podcast.artwork}" />
 
           <meta name="twitter:card" content="player">

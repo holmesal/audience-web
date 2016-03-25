ce5ae2a6217e43b6d05ad1b4ee64352e9249242a
Alonso Holmes <hello@alonso.io>
2016-03-25 15:17:39 -0700

    added staging constant

--- dist/constants.js
+++ dist/constants.js
@@ -6,5 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 var S3_BUCKET = exports.S3_BUCKET = process.env.NODE_ENV === 'production' ? 'audience-clips' : 'audience-clips-dev';
 console.info('S3_BUCKET: ' + S3_BUCKET);
 
-var GRAPHQL_URL = exports.GRAPHQL_URL = process.env.NODE_ENV === 'production' ? 'https://podcastfoo.herokuapp.com/graphql-public' : 'http://localhost:5000/graphql-public';
+var isStaging = exports.isStaging = process.env.IS_STAGING;
+
+var GRAPHQL_URL = exports.GRAPHQL_URL = process.env.NODE_ENV === 'production' ? isStaging ? 'https://podcastfoo.herokuapp.com/graphql-public' : 'https://podcastfoo.herokuapp.com/graphql-public' : 'http://localhost:5000/graphql-public';
 console.info('GRAPHQL_URL: ' + GRAPHQL_URL);
--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,9 @@ export const S3_BUCKET = process.env.NODE_ENV === 'production' ?
     'audience-clips-dev';
 console.info(`S3_BUCKET: ${S3_BUCKET}`);
 
+export const isStaging = process.env.IS_STAGING;
+
 export const GRAPHQL_URL = process.env.NODE_ENV === 'production' ?
-    'https://podcastfoo.herokuapp.com/graphql-public' :
+    (isStaging ? 'https://podcastfoo.herokuapp.com/graphql-public' : 'https://podcastfoo.herokuapp.com/graphql-public') :
     'http://localhost:5000/graphql-public';
 console.info(`GRAPHQL_URL: ${GRAPHQL_URL}`);
